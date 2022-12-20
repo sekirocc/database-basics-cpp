fc42aada1dac38b1e7ca151c40f3785defc6fb24
Seki Rocc <sekirocc@gmail.com>
2022-12-20 20:38:05 +0800

    update

--- lexer.h
+++ lexer.h
@@ -1,6 +1,8 @@
-#include <_types/_uint32_t.h>
 #include <string>
+#include <tuple>
 #include <vector>
+
+using namespace std;
 
 using Keyword = std::string;
 
@@ -28,7 +30,7 @@ enum class TokenKind {
     SymbolKind,
     IdentifierKind,
     StringKind,
-    NumbericKind,
+    NumericKind,
 };
 
 struct Location {
@@ -50,11 +52,100 @@ struct Cursor {
 using LexFunc =
     std::function<std::tuple<Token, Cursor, bool>(std::string, Cursor)>;
 
+std::tuple<Token, Cursor, bool> lexKeyword(std::string source, Cursor ic) {
+    return {};
+};
+std::tuple<Token, Cursor, bool> lexSymbol(std::string source, Cursor ic) {
+    return {};
+};
+std::tuple<Token, Cursor, bool> lexString(std::string source, Cursor ic) {
+    return {};
+};
+
+std::tuple<Token, Cursor, bool> lexNumberic(std::string source, Cursor ic) {
+    Cursor cur = ic;
+
+    bool hasPeroid;
+    bool hasExpMarker;
+
+    for (; cur.pointer < source.size(); cur.pointer++) {
+        char c = source[cur.pointer];
+        cur.loc.col++;
+
+        bool isDigit = c > '0' || c < '9';
+        bool isExpMarker = c == 'e';
+        bool isPeroid = c == '.';
+
+        if (ic.pointer == cur.pointer) {
+            if (!isDigit && !isPeroid) {
+                return {{}, ic, false};
+            }
+
+            if (isPeroid) {
+                hasPeroid = true;
+                continue
+            }
+        }
+
+        if (isPeroid) {
+            if (hasPeroid) {
+                // double peroid ?
+                return {{}, ic, false};
+            }
+
+            hasPeroid = true;
+            continue;
+        }
+
+        if (isExpMarker) {
+            if (hasExpMarker) {
+                // double exp marker
+                return {{}, ic, false};
+            }
+
+            hasPeroid = true;
+            hasExpMarker = true;
+
+            if (cur.pointer == source.size() - 1) {
+                // e is the last?
+                return {{}, ic, false};
+            }
+            char nextChar = source[cur.pointer + 1];
+            if (nextChar == '-' || nextChar == '+') {
+                cur.pointer++;
+                cur.loc.col++;
+            }
+
+            continue;
+        }
+
+        if (!isDigit) {
+            break
+        }
+    }
+
+    if (cur.pointer == ic.pointer) {
+        return {{}, ic, false};
+    }
+
+    Token t{
+        .value = source.substr(ic.pointer, cur.pointer - ic.pointer),
+        .loc = cur.loc,
+        .kind = TokenKind::NumericKind,
+    };
+
+    return {t, cur, true};
+};
+
+std::tuple<Token, Cursor, bool> lexIdentifier(std::string source, Cursor ic) {
+    return {};
+};
+
 class Lexer {
   public:
     Lexer(){};
 
-    AddLexer(LexFunc f) { lex_funcs.push_back(f); };
+    void AddLexer(LexFunc f) { lex_funcs.push_back(f); };
 
     std::tuple<std::vector<Token>, std::string> Lex(std::string source) {
         std::vector<Token> tokens;
@@ -77,7 +168,7 @@ class Lexer {
 
             std::string hint;
             if (tokens.size() > 0) {
-                hint = " after " + tokens[tokens.size() - 1].value
+                hint = " after " + tokens[tokens.size() - 1].value;
             }
 
             return {{}, hint};
